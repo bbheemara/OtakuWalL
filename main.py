e7b7ee4aa35c5917dfccb3e7d12f7fc60bed46bb
bbheemara <bbheemaraya944@gmail.com>
2025-09-14 20:28:06 +0530

    Modified ui, conncted ui.py and main.py

--- main.py
+++ main.py
@@ -7,187 +7,169 @@ from api.fetch_quote import quotes
 from dotenv import load_dotenv
 from io import BytesIO
 
-load_dotenv()
+def set_wallpaper(type="Anime",category="Anime",city="Mumbai",use_time_greetings=True):
+        load_dotenv()
 
-SPI_SETDESKWALLPAPER = 20
-SAVE_DIR = 'assets/backgrounds'
-WALLPAPER_FILENAME = 'wallpaper.jpg'
-FONT_PATH = 'assets/fonts/google_font.ttf'
-font=ImageFont.truetype(FONT_PATH,size=50)
-os.makedirs(SAVE_DIR, exist_ok=True)
-message = getMessage()
-
-
-try:
-    quote, anime_name, character = quotes()
-except Exception as e:
-    print("Failed to fetch quote:", e)
-    quote = "I'm not gonna run away, I never go back on my word! That's my nindo: my ninja way!"
-    anime_name = "Naruto"
-    character = "Naruto Uzumaki"
-
-anime_name=anime_name
+        SPI_SETDESKWALLPAPER = 20
+        SAVE_DIR = 'assets/backgrounds'
+        WALLPAPER_FILENAME = 'wallpaper.jpg'
+        FONT_PATH = 'assets/fonts/google_font.ttf'
+        font=ImageFont.truetype(FONT_PATH,size=50)
+        os.makedirs(SAVE_DIR, exist_ok=True)
+        message = getMessage()
 
 
-def download_wallpaper(image_url, filename=WALLPAPER_FILENAME, save_dir=SAVE_DIR):
-    response = requests.get(image_url)
-    if response.status_code == 200:
-        filepath = os.path.join(save_dir, filename)
-        with open(filepath, 'wb') as f:
-            f.write(response.content)
-        return filepath
-    else:
-        print("Failed to download wallpaper.")
-        return None   
+        try:
+            quote, anime_name, character = quotes()
+        except Exception as e:
+            print("Failed to fetch quote:", e)
+            quote = "I'm not gonna run away, I never go back on my word! That's my nindo: my ninja way!"
+            anime_name = "Naruto"
+            character = "Naruto Uzumaki"
+
+        # anime_name=anime_name
 
 
-
-def fetch_wallpaper():
-    url = f'https://wallhaven.cc/api/v1/search?q={anime_name}&categories=anime&purity=100&sorting=random&resolutions=1920x1080&ratios=16x9'
-    resp = requests.get(url)
-    data = resp.json()
-    if 'data' in data and len(data['data']) > 0:
-        image_url = data['data'][0]['path']
-        return download_wallpaper(image_url)
-    else:
-        print("No wallpaper found:Retrting")
-        resp = requests.get(url)
-        data = resp.json()
-        if 'data' in data and len(data['data']) > 0:
-            image_url = data['data'][0]['path']
-            return download_wallpaper(image_url)
-        
-
-
-def wrap_text(text, font, max_width, draw):
-    words = text.split()
-    lines = []
-    line = ""
-
-    for word in words:
-        test_line = f"{line} {word}".strip()
-        bbox = draw.textbbox((0, 0), test_line, font=font)
-        w = bbox[2] - bbox[0]
-        if w <= max_width:
-            line = test_line
-        else:
-            lines.append(line)
-            line = word
-    if line:
-        lines.append(line)
-    return lines
+        def download_wallpaper(image_url, filename=WALLPAPER_FILENAME, save_dir=SAVE_DIR):
+            response = requests.get(image_url)
+            if response.status_code == 200:
+                filepath = os.path.join(save_dir, filename)
+                with open(filepath, 'wb') as f:
+                    f.write(response.content)
+                return filepath
+            else:
+                print("Failed to download wallpaper.")
+                return None   
 
 
 
-def draw_outline_text(draw, text, position, font, fill=(255,255,255), outline=(0,0,0)):
-    x, y = position
-    for dx in [-2, 0, 2]:
-        for dy in [-2, 0, 2]:
-            draw.text((x + dx, y + dy), text, font=font, fill=outline)
-    draw.text((x, y), text, font=font, fill=fill)
+        def fetch_wallpaper():
+            url = f'https://wallhaven.cc/api/v1/search?q={type}&categories={category}&purity=100&sorting=random&resolutions=1920x1080&ratios=16x9'
+            resp = requests.get(url)
+            data = resp.json()
+            if 'data' in data and len(data['data']) > 0:
+                image_url = data['data'][0]['path']
+                return download_wallpaper(image_url)
+            else:
+                print("No wallpaper found:Retrting")
+                resp = requests.get(url)
+                data = resp.json()
+                if 'data' in data and len(data['data']) > 0:
+                    image_url = data['data'][0]['path']
+                    return download_wallpaper(image_url)
+                
+
+
+        def wrap_text(text, font, max_width, draw):
+            words = text.split()
+            lines = []
+            line = ""
+
+            for word in words:
+                test_line = f"{line} {word}".strip()
+                bbox = draw.textbbox((0, 0), test_line, font=font)
+                w = bbox[2] - bbox[0]
+                if w <= max_width:
+                    line = test_line
+                else:
+                    lines.append(line)
+                    line = word
+            if line:
+                lines.append(line)
+            return lines
 
 
 
-def draw_wrapped_text(draw, text, position, font, max_width):
-    x, y = position
-    lines = wrap_text(text, font, max_width, draw)
-    for line in lines:
-        draw_outline_text(draw, line, (x, y), font)
-        bbox = draw.textbbox((x, y), line, font=font)
-        line_height = bbox[3] - bbox[1]
-        y += line_height + 5
-
-
-downloaded_path = fetch_wallpaper()
-if not downloaded_path:
-    exit()
-
-
-image = Image.open(downloaded_path).convert("RGB")
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype(FONT_PATH, 50)
-
-
-draw_wrapped_text(draw, message, (100, 40), font, image.width - 60)
-
-
-quote_text = f'"{quote}"\n- {character}'
-quote_lines = wrap_text(quote_text, font, image.width - 100, draw)
-
-
-line_heights = [draw.textbbox((0, 0), line, font=font)[3] - draw.textbbox((0, 0), line, font=font)[1] for line in quote_lines]
-total_quote_height = sum(line_heights) + 5 * (len(quote_lines) - 1)
-
-start_y =  image.height - total_quote_height - 100
+        def draw_outline_text(draw, text, position, font, fill=(255,255,255), outline=(0,0,0)):
+            x, y = position
+            for dx in [-2, 0, 2]:
+                for dy in [-2, 0, 2]:
+                    draw.text((x + dx, y + dy), text, font=font, fill=outline)
+            draw.text((x, y), text, font=font, fill=fill)
 
 
 
-
-for i, line in enumerate(quote_lines):
-    line_bbox = draw.textbbox((0, 0), line, font=font)
-    line_width = line_bbox[2] - line_bbox[0]
-    x = (image.width - line_width) // 2
-    draw_outline_text(draw, line, (x, start_y), font)
-    start_y += line_heights[i] + 5            
-
-image = image.convert('RGBA') 
-API_KEY=os.environ.get('api')
-city='Raichur'
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
-resp_w=requests.get(url)
-data_w=resp_w.json()
-
-if resp_w.status_code==200:
-    temp=data_w['main']['temp']
-    weather_state=data_w['weather'][0]['main']
-    icon_code=data_w['weather'][0]['icon']
-    icon_url = f"https://openweathermap.org/img/wn/{icon_code}@2x.png"
-    resp=requests.get(icon_url)
-    icon_img = Image.open(BytesIO(resp.content)).convert("RGBA")
-    
-
-    temp=temp
-    temp_text=f'{temp}°C'
-    if weather_state=='Clouds':
-       
-    #    weather = Image.open('assets/clouds.png').convert("RGBA") 
-       icon_img = icon_img.resize((50, 50))
-       alpha =200
-       icon_img.putalpha(alpha)
-       draw = ImageDraw.Draw(image)
-       image.paste(icon_img, (image.width-200, 50), icon_img)  
-       draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
-       draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
-
-    elif weather_state=='Clear':
-          icon_img = icon_img.resize((50, 50))
-          alpha =200
-          icon_img.putalpha(alpha)
-          draw = ImageDraw.Draw(image)
-          image.paste(icon_img, (image.width-200, 50), icon_img)  
-          draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
-          draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
-
-    elif weather_state=='Rain':
-          icon_img = icon_img.resize((50, 50))
-          alpha =200
-          icon_img.putalpha(alpha)
-          draw = ImageDraw.Draw(image)
-          image.paste(icon_img, (image.width-200, 50), icon_img)  
-          draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
-          draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
+        def draw_wrapped_text(draw, text, position, font, max_width):
+            x, y = position
+            lines = wrap_text(text, font, max_width, draw)
+            for line in lines:
+                draw_outline_text(draw, line, (x, y), font)
+                bbox = draw.textbbox((x, y), line, font=font)
+                line_height = bbox[3] - bbox[1]
+                y += line_height + 5
 
 
-    elif weather_state=='Drizzle':
-            icon_img = icon_img.resize((50, 50))
-            alpha =200
-            icon_img.putalpha(alpha)
-            draw = ImageDraw.Draw(image)
-            image.paste(icon_img, (image.width-200, 50), icon_img)  
-            draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
-            draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
+        downloaded_path = fetch_wallpaper()
+        if not downloaded_path:
+            exit()
 
-    elif weather_state=='Snow':
+
+        image = Image.open(downloaded_path).convert("RGB")
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype(FONT_PATH, 50)
+
+
+        draw_wrapped_text(draw, message, (100, 40), font, image.width - 60)
+
+
+        quote_text = f'"{quote}"\n- {character}'
+        quote_lines = wrap_text(quote_text, font, image.width - 100, draw)
+
+
+        line_heights = [draw.textbbox((0, 0), line, font=font)[3] - draw.textbbox((0, 0), line, font=font)[1] for line in quote_lines]
+        total_quote_height = sum(line_heights) + 5 * (len(quote_lines) - 1)
+
+        start_y =  image.height - total_quote_height - 100
+
+
+
+
+        for i, line in enumerate(quote_lines):
+            line_bbox = draw.textbbox((0, 0), line, font=font)
+            line_width = line_bbox[2] - line_bbox[0]
+            x = (image.width - line_width) // 2
+            draw_outline_text(draw, line, (x, start_y), font)
+            start_y += line_heights[i] + 5            
+
+        image = image.convert('RGBA') 
+        API_KEY=os.environ.get('api')
+        city='Mumbai'
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+        resp_w=requests.get(url)
+        data_w=resp_w.json()
+
+        if resp_w.status_code==200:
+            temp=data_w['main']['temp']
+            weather_state=data_w['weather'][0]['main']
+            icon_code=data_w['weather'][0]['icon']
+            icon_url = f"https://openweathermap.org/img/wn/{icon_code}@2x.png"
+            resp=requests.get(icon_url)
+            icon_img = Image.open(BytesIO(resp.content)).convert("RGBA")
+            
+
+            temp=temp
+            temp_text=f'{temp}°C'
+            if weather_state=='Clouds':
+            
+            #    weather = Image.open('assets/clouds.png').convert("RGBA") 
+                icon_img = icon_img.resize((50, 50))
+                alpha =200
+                icon_img.putalpha(alpha)
+                draw = ImageDraw.Draw(image)
+                image.paste(icon_img, (image.width-200, 50), icon_img)  
+                draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
+                draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
+
+            elif weather_state=='Clear':
+                icon_img = icon_img.resize((50, 50))
+                alpha =200
+                icon_img.putalpha(alpha)
+                draw = ImageDraw.Draw(image)
+                image.paste(icon_img, (image.width-200, 50), icon_img)  
+                draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
+                draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
+
+            elif weather_state=='Rain':
                 icon_img = icon_img.resize((50, 50))
                 alpha =200
                 icon_img.putalpha(alpha)
@@ -197,10 +179,30 @@ if resp_w.status_code==200:
                 draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
 
 
-else:
-    print("Error:", data_w['message'])
+            elif weather_state=='Drizzle':
+                    icon_img = icon_img.resize((50, 50))
+                    alpha =200
+                    icon_img.putalpha(alpha)
+                    draw = ImageDraw.Draw(image)
+                    image.paste(icon_img, (image.width-200, 50), icon_img)  
+                    draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
+                    draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
+
+            elif weather_state=='Snow':
+                        icon_img = icon_img.resize((50, 50))
+                        alpha =200
+                        icon_img.putalpha(alpha)
+                        draw = ImageDraw.Draw(image)
+                        image.paste(icon_img, (image.width-200, 50), icon_img)  
+                        draw_wrapped_text(draw, weather_state, (image.width - 400 + 50, 100), font, image.width - 60)
+                        draw_wrapped_text(draw,temp_text, (image.width-400,50),font,image.width-60)
 
 
-bmp_path = downloaded_path.replace('.jpg', '.bmp')
-image.save(bmp_path, 'BMP')
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, os.path.abspath(bmp_path), 3)
+        else:
+            print("Error:", data_w['message'])
+
+
+        bmp_path = downloaded_path.replace('.jpg', '.bmp')
+        image.save(bmp_path, 'BMP')
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, os.path.abspath(bmp_path), 3)
+        pass
--- ui.py
+++ ui.py
@@ -2,12 +2,12 @@ import customtkinter as ctk
 import tkinter as tk
 import tkinter.font as tkfont
 import os 
-
+from main import set_wallpaper
 
 ctk.set_appearance_mode('system')
 ctk.set_default_color_theme('green')
 root = ctk.CTk()
-root.geometry("600x600")
+root.geometry("500x500")
 
 
 
@@ -15,27 +15,32 @@ frame = ctk.CTkFrame(master=root)
 frame.pack(padx= 20, pady = 10, expand=True, fill="both")
 
 container_Type_wallpaper_label= ctk.CTkFrame(frame,fg_color='transparent')
-container_Type_wallpaper_label.pack(padx=20,pady=5)
-
-
-container_time_based_greetings_label = ctk.CTkFrame(frame,fg_color='transparent')
-container_time_based_greetings_label.pack(padx=20,pady=5)
-
-container_city_input_label = ctk.CTkFrame(frame,fg_color='transparent')
-container_city_input_label.pack(padx=20,pady=5)
+container_Type_wallpaper_label.pack(padx=20,pady=10)
 
 container_Type_quote_label = ctk.CTkFrame(frame,fg_color='transparent')
-container_Type_quote_label.pack(padx=20,pady=5)
+container_Type_quote_label.pack(padx=20,pady=10)
 
+container_city_input_label = ctk.CTkFrame(frame,fg_color='transparent')
+container_city_input_label.pack(padx=20,pady=10)
+
+container_time_based_greetings_label = ctk.CTkFrame(frame,fg_color='transparent')
+container_time_based_greetings_label.pack(padx=20,pady=10)
 
 
 
 ctk.FontManager.load_font(r"assets/fonts/font1.ttf")
 
 
+def wallpaper_set():
+    wallpaper_type = optionmenu_wallpaper.get()
+    quote_type = optionmenu_quote.get()
+    city_name = city_input.get()
+    is_greetings = time_based_greetings_label.get()
 
+    set_wallpaper(type=wallpaper_type,city=city_name,use_time_greetings=is_greetings)
+    
 
-Header = ctk.CTkLabel(container_Type_wallpaper_label,text='OtakuWal',font=("Oswald Light", 24))
+Header = ctk.CTkLabel(container_Type_wallpaper_label,text='OtakuWalL',font=("Oswald Light", 24))
 Header.pack()
 def options_menu(choice):
     print("optionmenu dropdown clicked:", choice)
@@ -52,9 +57,9 @@ quote_label = ctk.CTkLabel(container_Type_quote_label,text='Quote (optional):',f
 quote_label.pack(side='left')
 
 
-optionmenu_quote = ctk.CTkOptionMenu(container_Type_quote_label,values=["Anime","Movie","Nature","Select"],font=("Oswald ExtraLight", 15))
+optionmenu_quote = ctk.CTkOptionMenu(container_Type_quote_label,values=["Anime Quotes","Movie Quotes","Famouse Quotes","Select"],font=("Oswald ExtraLight", 15))
 optionmenu_quote.set("Select")
-optionmenu_quote.pack(side='left',padx=5)
+optionmenu_quote.pack(side='left',padx=10)
 
 city_label = ctk.CTkLabel(container_city_input_label,text='Enter city for weather (optional):',font=("Oswald ExtraLight",15))
 city_label.pack(side = 'left')
@@ -68,7 +73,8 @@ city_input.pack(side='left', padx=5)
 
 time_based_greetings_label = ctk.CTkCheckBox(container_time_based_greetings_label,text='Time based greetings',font=("Oswald ExtraLight", 15))
 time_based_greetings_label.pack(pady=5)
-# btn = ctk.CTkButton(master=frame,text='Set!!')
-# btn.pack(padx=20,pady=60)
+
+btn = ctk.CTkButton(master=frame,text='Set!!',command=wallpaper_set)
+btn.pack(padx=20,pady=30)
 
 root.mainloop()
